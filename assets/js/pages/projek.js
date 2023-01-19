bb2d8379502aaf1436805d8edf8290b10e2a4bf8
saka_mawa <sakadana2003@gmail.com>
2023-01-19 17:01:10 +0700

    PUSH

---
+++ assets/js/pages/projek.js
@@ -0,0 +1,16 @@
+$("#kt_daterangepicker_1").daterangepicker();
+
+$(function() {
+    $("#kt_table_projek").on("click", function() {
+      $("#deleteall").toggle($(this).find(".deletebox:checked").length > 0);
+      if($(this).find(".deletebox:checked").length < ($(this).find(".deletebox").length)){
+          $('input[name="maincheckbox"]').prop('checked', false);
+      }else if($(this).find(".deletebox:checked").length = ($(this).find(".deletebox").length)){
+        $('input[name="maincheckbox"]').prop('checked', true);
+    }
+  })
+  $('input[name="maincheckbox"]').on("click", function() {
+      $('.deletebox').prop('checked', this.checked);
+  });
+  
+});
